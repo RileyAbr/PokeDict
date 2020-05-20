5e4e7298125df4207f3f9c69028e43415f85ec6b
Riley Abrahamson <rabrahamson@bushelpowered.com>
2020-05-20 16:38:28 -0500

    Brought in data for Detail page

--- src/components/Detail/index.js
+++ src/components/Detail/index.js
@@ -1,15 +1,44 @@
 import React from "react";
+import axios from "axios";
+
+import "./styles.css";
 
 class Detail extends React.Component {
+  state = {
+    name: this.props.location.pathname.slice(9), //
+    pokemon: "",
+  };
+
   goBack = () => {
     this.props.history.goBack();
   };
+
+  getPokemon = () => {
+    axios
+      .get("https://intern-pokedex.myriadapps.com/api/v1/pokemon/", {
+        params: {
+          name: this.state.name,
+        },
+      })
+      .then((response) => {
+        const pokemon = response.data.data[0]; // The first 'data' refers to the value within the axios response. The second refers to the data key in the API response.
+        // The API returns an array of pokemom. When there is only a single match, it will be at index 0
+        this.setState({ pokemon });
+      });
+  };
+
+  componentDidMount() {
+    this.getPokemon();
+    console.log(this.state.name);
+  }
 
   render() {
     return (
       <div>
         <button onClick={this.goBack}>Go Back</button>
         Detail Loaded
+        {this.state.pokemon.name}
+        <img src={this.state.pokemon.image} alt={this.state.pokemon.name} />
       </div>
     );
   }
