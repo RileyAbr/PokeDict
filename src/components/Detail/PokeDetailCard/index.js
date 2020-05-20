7452b24c82f3d4eae43ce8dc51a8c1fcfcae0848
Riley Abrahamson <rabrahamson@bushelpowered.com>
2020-05-20 17:07:13 -0500

    Added styles to PokeDetailCard header area

--- src/components/Detail/PokeDetailCard/index.js
+++ src/components/Detail/PokeDetailCard/index.js
@@ -5,14 +5,18 @@ import "./styles.css";
 function PokeDetailCard(props) {
   return (
     <div className="detail-card">
-      <h2>{props.name}</h2>
-      <h3>{props.id}</h3>
-      <div>
-        {/* TODO: Reading just props.types works, but if you try to access the types with [0] or .map(), it says it is undefined? */}
-        {props.types}
+      <div className="detail-card-header">
+        <div>
+          <h2 className="detail-card-name">{props.name}</h2>
+          <h3 className="detail-card-id">#{props.id}</h3>
+        </div>
+        <div>
+          {/* TODO: Reading just props.types works, but if you try to access the types with [0] or .map(), it says it is undefined? */}
+          {props.types}
 
-        {/* {props.types[0] != null && <div className="a">{props.types[0]}</div>}
-        {props.types[1] != null && <div className="a">{props.types[1]}</div>} */}
+          {/* {props.types[0] != null && <div className="a">{props.types[0]}</div>}
+            {props.types[1] != null && <div className="a">{props.types[1]}</div>} */}
+        </div>
       </div>
       <img src={props.image} alt={props.name} />
     </div>
