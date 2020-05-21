123a0d2a9af213b0e6871a8caa3bb7fb0eb49f0a
Riley Abrahamson <rabrahamson@bushelpowered.com>
2020-05-21 11:39:52 -0500

    Added all HTML and majority of styles for PokeDetailCard

--- src/components/Detail/PokeDetailCard/index.js
+++ src/components/Detail/PokeDetailCard/index.js
@@ -18,7 +18,76 @@ function PokeDetailCard(props) {
             {props.types[1] != null && <div className="a">{props.types[1]}</div>} */}
         </div>
       </div>
-      <img src={props.image} alt={props.name} />
+      <div className="detail-card-body">
+        <img
+          className="detail-card-sprite"
+          src={props.image}
+          alt={props.name}
+        />
+        <div className="detail-card-stats">Pokemon.Stats</div>
+
+        {/* Bio section */}
+        <section className="detail-card-bio">
+          <h2 className="detail-card-classification">
+            Pokemon.Classification Pokemon
+          </h2>
+          <p className="detail-card-description">
+            Pokemon.description Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Sed et pellentesque arcu, ac pulvinar erat. Morbi
+            ante turpis, dictum a dictum sed, posuere ac ligula. Etiam mi diam,
+            commodo sed felis rhoncus, egestas sodales est.
+          </p>
+        </section>
+
+        {/* Profile section */}
+        <h4 className="detail-card-section-header">Profile</h4>
+        <section className="detail-card-profile">
+          <table>
+            <tbody>
+              <tr>
+                <td>Height:</td>
+                <td>height</td>
+              </tr>
+              <tr>
+                <td>Catch Rate:</td>
+                <td>catchRate</td>
+              </tr>
+              <tr>
+                <td>Egg Groups:</td>
+                <td>eggGroups</td>
+              </tr>
+              <tr>
+                <td>Abilities</td>
+                <td>abilities</td>
+              </tr>
+            </tbody>
+          </table>
+
+          <table>
+            <tbody>
+              <tr>
+                <td>Weight:</td>
+                <td>weight</td>
+              </tr>
+              <tr>
+                <td>Gender Ratio:</td>
+                <td>genderRatio</td>
+              </tr>
+              <tr>
+                <td>Hatch Steps:</td>
+                <td>hatchSteps</td>
+              </tr>
+              <tr>
+                <td>EVs:</td>
+                <td>evs</td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+
+        <h4 className="detail-card-section-header">Damage When Attacked</h4>
+        <section></section>
+      </div>
     </div>
   );
 }
