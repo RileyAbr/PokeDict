266bfcd1c6b9971b67314e76bdfb25f7af4d6205
Riley Abrahamson <rabrahamson@bushelpowered.com>
2020-06-22 16:25:00 -0500

    Page calculation moved to a new Utility file

---
+++ src/Utils.js
@@ -0,0 +1,21 @@
+// Calculates which page should be navigated to in reverse
+export const backPageCalculation = (currentPage, maxPages) => {
+  if (currentPage <= 1) {
+    return 1;
+  } else if (currentPage <= maxPages) {
+    return currentPage - 1;
+  } else {
+    return maxPages;
+  }
+};
+
+// Calculates which page should be navigated to going forward
+export const forwardPageCalculation = (currentPage, maxPages) => {
+  if (currentPage >= maxPages) {
+    return maxPages;
+  } else if (currentPage >= 1) {
+    return currentPage + 1;
+  } else {
+    return 1;
+  }
+};
