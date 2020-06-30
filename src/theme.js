ada8076c3d4c35ec693cba4c715d98b1f603a656
Riley Abrahamson <rabrahamson@bushelpowered.com>
2020-06-30 13:41:41 -0500

    Added type specific colors to the theme.js file

--- src/theme.js
+++ src/theme.js
@@ -5,6 +5,7 @@ export default {
     bgCardWhite: "#ffffff",
 
     /* Borders */
+    borderWhite: "#ffffff",
     borderGrey: "#e9e9e9",
 
     /* Inputs */
@@ -14,6 +15,49 @@ export default {
     fontWhite: "#ffffff",
     fontGrey: "#cdcdcd",
     fontDarkGreen: "#458a80",
+
+    // Type Colors
+    typeColors: {
+      bug: "#A8B820",
+      dark: "#705848",
+      dragon: "#7038F8",
+      electric: "#F8D030",
+      fairy: "#EE99AC",
+      fighting: "#C03028",
+      fire: "#F08030",
+      flying: "#A890F0",
+      ghost: "#705898",
+      grass: "#78C850",
+      ground: "#E0C068",
+      ice: "#98D8D8",
+      normal: "#C6C6A7",
+      poison: "#A040A0",
+      psychic: "#F85888",
+      rock: "#B8A038",
+      steel: "#B8B8D0",
+      water: "#6890F0",
+    },
+
+    typeBgColors: {
+      bug: "#eff3d8",
+      dark: "#c7bcb8",
+      dragon: "#cbb5fc",
+      electric: "#fdf4ce",
+      fairy: "#fce9ed",
+      fighting: "#e8b3b0",
+      fire: "#f9d3b9",
+      flying: "#ede9fc",
+      ghost: "#d7d0e2",
+      grass: "#d2edc4",
+      ground: "#f6edd5",
+      ice: "#edf8f8",
+      normal: "#FFFFFF",
+      poison: "#eeddee",
+      psychic: "#fdcedc",
+      rock: "#ebe5c7",
+      steel: "#f0f0f5",
+      water: "#e8edfc",
+    },
   },
   fonts: {
     fontStandard: "Verdana, Geneva, sans-serif",
