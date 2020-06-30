6310e45e21125d167c655ee61883b242aaa94aca
Riley Abrahamson <rabrahamson@bushelpowered.com>
2020-06-30 13:04:35 -0500

    Implemented styled-system and initialized a theme

---
+++ src/theme.js
@@ -0,0 +1,21 @@
+export default {
+  colors: {
+    // Background
+    bgHomeGreen: "#55a69c",
+    bgCardWhite: "#ffffff",
+
+    /* Borders */
+    borderGrey: "#e9e9e9",
+
+    /* Inputs */
+    buttonGreen: "#4e988f",
+    searchGreen: "#509f95",
+
+    fontWhite: "#ffffff",
+    fontGrey: "#cdcdcd",
+    fontDarkGreen: "#458a80",
+  },
+  fonts: {
+    fontStandard: "Verdana, Geneva, sans-serif",
+  },
+};
