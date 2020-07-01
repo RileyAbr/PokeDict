b6217c033d503c2f3051ba09db9cd0a1ef7fffad
Riley Abrahamson <rabrahamson@bushelpowered.com>
2020-07-01 15:22:20 -0500

    Converted PokeCard component to styled-componenets

--- src/components/PokeCard/index.js
+++ src/components/PokeCard/index.js
@@ -1,24 +1,51 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
-import "./styles.css";
+import styled from "styled-components";
+import { color, typography, border } from "styled-system";
 
 // Styled elements
 import Types from "../../styled-components/Types";
 
+const Card = styled.article`
+  ${color};
+  ${typography};
+  width: 275px;
+  margin: 15px 10px;
+  border-radius: 3px;
+`;
+
+const CardName = styled.h2`
+  font-size: 1.1rem;
+  padding: 1.3rem 2rem;
+  font-weight: bold;
+  border: solid 1px black;
+  ${border};
+`;
+
+const CardSprite = styled.img`
+  display: block;
+  margin: 0 auto;
+  height: 225px;
+`;
+
+// Main Component
 function PokeCard(props) {
   return (
     <Link to={`/pokemon/${props.name}`}>
-      <article className="card">
+      <Card bg={"bg.cardWhite"} fontFamily={"fontStandard"}>
         {/* Pokemon name */}
-        <h2 className="card-name">{props.name}</h2>
+        <CardName borderColor={"border.grey"}>{props.name}</CardName>
 
         {/* Pokemon Sprite */}
-        <img className="card-sprite" src={props.image} alt={props.name} />
+        <CardSprite
+          className="card-sprite"
+          src={props.image}
+          alt={props.name}
+        />
 
         {/* Pokemon Types */}
         <Types firstType={props.types[0]} secondType={props.types[1]} />
-      </article>
+      </Card>
     </Link>
   );
 }
