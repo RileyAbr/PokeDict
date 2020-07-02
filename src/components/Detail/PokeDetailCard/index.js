65e125f426f077346ce2f8006240074d39ab3daf
Riley Abrahamson <rabrahamson@bushelpowered.com>
2020-07-02 11:44:44 -0500

    All components are now moved to a CSS-in-JS solutions!

--- src/components/Detail/PokeDetailCard/index.js
+++ src/components/Detail/PokeDetailCard/index.js
@@ -1,76 +1,166 @@
 import React from "react";
-
-import "./styles.css";
+import styled from "styled-components";
+import { color, border } from "styled-system";
 
 // Styled components
 import Types from "../../../styled-components/Types";
 
+const DetailCard = styled.article`
+  ${color};
+  max-width: 700px;
+  margin: auto;
+  border-radius: 3px;
+  padding: 10px;
+`;
+
+const CardHeader = styled.header`
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: baseline;
+  justify-content: space-between;
+  padding: 10px 10px 15px;
+  border-bottom: solid 1px black;
+  ${border};
+`;
+
+const CardName = styled.h2`
+  font-weight: bold;
+  display: inline;
+  margin-right: 0.5rem;
+`;
+
+const CardID = styled.h3`
+  ${color};
+  display: inline;
+`;
+
+const CardBody = styled.div`
+  padding: 0 25px;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-around;
+`;
+
+const CardSprite = styled.img`
+  width: 200px;
+  height: auto;
+`;
+
+const CardStats = styled.div`
+  /* Testing values */
+  border: 2px solid black;
+  ${border};
+  border-radius: 3px;
+  width: 200px;
+`;
+
+const CardBio = styled.section`
+  padding: 0px 20px;
+`;
+
+const CardGenus = styled.h2`
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+`;
+
+const CardDescription = styled.p`
+  line-height: 1.2rem;
+`;
+
+const CardSubheading = styled.h4`
+  text-align: left;
+  width: 100%;
+  ${color}
+  margin: 20px 0;
+  padding: 0.4rem 1rem;
+`;
+
+const CardProfile = styled.section`
+  display: flex;
+  flex-flow: row wrap;
+  padding: 4px;
+`;
+
+const CardTableItem = styled.td`
+  padding: 0.75rem 0;
+  padding-right: 35px;
+  &:nth-child(2n + 1) {
+    font-weight: bold;
+  }
+`;
+
+// Main componenet
 function PokeDetailCard(props) {
   return (
-    <div className="detail-card">
-      <div className="detail-card-header">
+    <DetailCard bg={"bg.cardWhite"}>
+      {/* Header */}
+      <CardHeader borderColor={"border.grey"}>
         <div>
-          <h2 className="detail-card-name">{props.pokemon.name}</h2>
-          <h3 className="detail-card-id">#{props.pokemon.id}</h3>
+          <CardName>{props.pokemon.name}</CardName>
+          <CardID color={"font.grey"}>#{props.pokemon.id}</CardID>
         </div>
         <Types
           firstType={props.pokemon.types[0]}
           secondType={props.pokemon.types[1]}
         />
-      </div>
-      <div className="detail-card-body">
-        <img
-          src={props.pokemon.image}
-          alt={props.pokemon.name}
-          className="detail-card-sprite"
-        />
-        <div className="detail-card-stats">
+      </CardHeader>
+
+      {/* Body */}
+      <CardBody>
+        <CardSprite src={props.pokemon.image} alt={props.pokemon.name} />
+        <CardStats borderColor={"border.grey"}>
           {props.pokemon.stats &&
             Object.entries(props.pokemon.stats).map(([key, value]) => (
-              <div>
+              <div key={key}>
                 {key}: {value}
               </div>
             ))}
-        </div>
+        </CardStats>
 
         {/* Bio section */}
-        <section className="detail-card-bio">
-          <h2 className="detail-card-classification">{props.pokemon.genus}</h2>
-          <p className="detail-card-description">{props.pokemon.description}</p>
-        </section>
+        <CardBio>
+          <CardGenus>{props.pokemon.genus}</CardGenus>
+          <CardDescription>{props.pokemon.description}</CardDescription>
+        </CardBio>
 
         {/* Profile section */}
-        <h4 className="detail-card-section-header">Profile</h4>
-        <section className="detail-card-section detail-card-profile">
+        <CardSubheading color={"font.white"} bg={"bg.homeGreen"}>
+          Profile
+        </CardSubheading>
+        <CardProfile>
           <table>
             <tbody>
               <tr>
-                <td>Height:</td>
-                <td>{props.pokemon.height} m</td>
+                <CardTableItem>Height:</CardTableItem>
+                <CardTableItem>{props.pokemon.height} m</CardTableItem>
               </tr>
               <tr>
-                <td>Catch Rate:</td>
-                <td>catch_rate</td>
+                <CardTableItem>Catch Rate:</CardTableItem>
+                <CardTableItem>catch_rate</CardTableItem>
               </tr>
               <tr>
-                <td>Egg Groups:</td>
-                <td>
+                <CardTableItem>Egg Groups:</CardTableItem>
+                <CardTableItem>
                   {props.pokemon.egg_groups
                     ? props.pokemon.egg_groups.map((element) => (
-                        <div>{element}</div>
+                        <div key={element}>
+                          {element.charAt(0).toUpperCase() + element.slice(1)}
+                        </div>
                       ))
                     : ""}
-                </td>
+                </CardTableItem>
               </tr>
               <tr>
-                <td>Abilities</td>
-                <td>
+                <CardTableItem>Abilities</CardTableItem>
+                <CardTableItem>
                   {props.pokemon.abilities
                     ? props.pokemon.abilities.map((element) => (
-                        <div>{element}</div>
+                        <div key={element}>
+                          {element.charAt(0).toUpperCase() + element.slice(1)}
+                        </div>
                       ))
                     : ""}
-                </td>
+                </CardTableItem>
               </tr>
             </tbody>
           </table>
@@ -78,29 +168,30 @@ function PokeDetailCard(props) {
           <table>
             <tbody>
               <tr>
-                <td>Weight:</td>
-                <td>{props.pokemon.weight} kg</td>
+                <CardTableItem>Weight:</CardTableItem>
+                <CardTableItem>{props.pokemon.weight} kg</CardTableItem>
               </tr>
               <tr>
-                <td>Gender Ratio:</td>
-                <td>gender_ratio</td>
+                <CardTableItem>Gender Ratio:</CardTableItem>
+                <CardTableItem>gender_ratio</CardTableItem>
               </tr>
               <tr>
-                <td>Hatch Steps:</td>
-                <td>hatch_steps</td>
+                <CardTableItem>Hatch Steps:</CardTableItem>
+                <CardTableItem>hatch_steps</CardTableItem>
               </tr>
               <tr>
-                <td>EVs:</td>
-                <td>evs</td>
+                <CardTableItem>EVs:</CardTableItem>
+                <CardTableItem>evs</CardTableItem>
               </tr>
             </tbody>
           </table>
-        </section>
+        </CardProfile>
 
-        <h4 className="detail-card-section-header">Damage When Attacked</h4>
-        <section></section>
-      </div>
-    </div>
+        {/* Unused */}
+        {/* <h4 className="detail-card-section-header">Damage When Attacked</h4>
+        <section></section> */}
+      </CardBody>
+    </DetailCard>
   );
 }
 
