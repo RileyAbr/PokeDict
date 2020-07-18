5ba1cd96d8442de8f3bd7a52cdc826c958d14db8
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2020-07-18 13:56:47 -0500

    Updated footer attribution

--- src/styled-components/Footer.js
+++ src/styled-components/Footer.js
@@ -5,64 +5,68 @@ import { Link } from "react-router-dom";
 import { color } from "styled-system";
 
 const FooterBox = styled.div`
-  ${color}
-  line-height: 1.2rem;
-  margin-top: 15px;
-  padding: 11px 0;
-  display: flex;
-  flex-flow: row wrap;
-  justify-content: center;
-  position: fixed;
-  bottom: 0;
-  width: 100%;
-  min-height: 2.5rem;
+    ${color}
+    line-height: 1.2rem;
+    margin-top: 15px;
+    padding: 11px 0;
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    min-height: 2.5rem;
 `;
 
 const FooterInternalLink = styled(Link)`
-  text-decoration: underline;
-  display: inline-block;
-  word-wrap: normal;
-  &:hover,
-  &:focus,
-  &:active {
-    ${color}
-  }
+    text-decoration: underline;
+    display: inline-block;
+    word-wrap: normal;
+    &:hover,
+    &:focus,
+    &:active {
+        ${color}
+    }
 `;
 
 const FooterExternalLink = styled.a`
-  text-decoration: underline;
-  display: inline-block;
-  word-wrap: normal;
-  &:hover,
-  &:focus,
-  &:active {
-    ${color}
-  }
+    text-decoration: underline;
+    display: inline-block;
+    word-wrap: normal;
+    &:hover,
+    &:focus,
+    &:active {
+        ${color}
+    }
 `;
 
 const FooterDivider = styled.span`
-  margin: 0 0.2em;
-  &:before {
-    content: "|";
-  }
+    margin: 0 0.2em;
+    &:before {
+        content: "|";
+    }
 `;
 
 function Footer(props) {
-  return (
-    <FooterBox color={"font.white"} bg={"bg.inputs"}>
-      <FooterInternalLink color={"font.grey"} to={"/"}>
-        Home{" "}
-      </FooterInternalLink>{" "}
-      <FooterDivider />
-      Created By&nbsp;
-      <FooterExternalLink
-        color={"font.grey"}
-        href="https://www.rileyabrahamson.com"
-      >
-        Riley Abrahamson
-      </FooterExternalLink>
-    </FooterBox>
-  );
+    return (
+        <FooterBox color={"font.white"} bg={"bg.inputs"}>
+            <FooterInternalLink color={"font.grey"} to={"/"}>
+                Home{" "}
+            </FooterInternalLink>
+            <FooterDivider />
+            <FooterExternalLink color={"font.grey"} href="https://pokeapi.co/">
+                PokéAPI
+            </FooterExternalLink>
+            <FooterDivider />
+            Created By&nbsp;
+            <FooterExternalLink
+                color={"font.grey"}
+                href="https://www.rileyabrahamson.com"
+            >
+                Riley Abrahamson
+            </FooterExternalLink>
+        </FooterBox>
+    );
 }
 
 export default Footer;
