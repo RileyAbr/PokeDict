0260ce85bc0447ec0d5c7d919d91a77652264546
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2020-07-26 16:23:56 -0500

    Fixed transition error with BackButton box shadow

--- src/styled-components/BackButton.js
+++ src/styled-components/BackButton.js
@@ -6,62 +6,63 @@ import { color, layout, typography } from "styled-system";
 import ArrowIcon from "./ArrowIcon";
 
 const Button = styled.button`
-  ${layout};
-  display: flex;
-  flex-flow: row nowrap;
-  align-items: center;
-  border-radius: 50px;
-  padding: 14px;
-  top: 10px;
-  left: 10px;
-  border: none;
-  text-align: center;
-  ${color};
-  cursor: pointer;
-  position: absolute;
-  /* Hover and click animations */
-  box-shadow: 0 5px
-    ${(props) => {
-      return props.theme.colors.bg.inputsShadow;
-    }};
-  &:hover,
-  &:focus {
-    box-shadow: 0 3px
-      ${(props) => {
-        return props.theme.colors.bg.inputsShadow;
-      }};
-  }
-  &:active {
-    box-shadow: none;
-  }
+    ${layout};
+    display: flex;
+    flex-flow: row nowrap;
+    align-items: center;
+    border-radius: 50px;
+    padding: 14px;
+    top: 10px;
+    left: 10px;
+    border: none;
+    text-align: center;
+    ${color};
+    cursor: pointer;
+    position: absolute;
+    /* Hover and click animations */
+    box-shadow: 0 5px
+        ${(props) => {
+            return props.theme.colors.bg.inputsShadow;
+        }};
+    &:hover,
+    &:focus {
+        box-shadow: 0 3px
+            ${(props) => {
+                return props.theme.colors.bg.inputsShadow;
+            }};
+    }
+    &:active {
+        box-shadow: none;
+    }
+    transition: background 0.75s ease-in-out, box-shadow 0.75s ease-in-out;
 `;
 
 const BackLabel = styled.span`
-  ${color}
-  ${typography}
+    ${color}
+    ${typography}
 `;
 
 function BackButton(props) {
-  const history = useHistory();
+    const history = useHistory();
 
-  return (
-    <Button
-      bg={"bg.inputs"}
-      fontFamily={"fontStandard"}
-      onClick={() => history.goBack()}
-    >
-      <ArrowIcon
-        right={props.right}
-        padding={["4px", "5px"]}
-        borderColor={"border.white"}
-        borderBottomWidth={[4, 5]}
-        borderRightWidth={[4, 5]}
-      />
-      <BackLabel fontSize={["1.3rem", "1.6rem"]} color={"font.white"}>
-        Back
-      </BackLabel>
-    </Button>
-  );
+    return (
+        <Button
+            bg={"bg.inputs"}
+            fontFamily={"fontStandard"}
+            onClick={() => history.goBack()}
+        >
+            <ArrowIcon
+                right={props.right}
+                padding={["4px", "5px"]}
+                borderColor={"border.white"}
+                borderBottomWidth={[4, 5]}
+                borderRightWidth={[4, 5]}
+            />
+            <BackLabel fontSize={["1.3rem", "1.6rem"]} color={"font.white"}>
+                Back
+            </BackLabel>
+        </Button>
+    );
 }
 
 export default BackButton;
